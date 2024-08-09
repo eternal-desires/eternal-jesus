366c4760fd32b1e44ae11a88c2de07e7682b11b6
eternal-desires <liam.furlong0118@gmail.com>
2024-08-09 15:37:02 -0400

    initial commit

---
+++ src/client/java/com/eternity/jessemood/client/JessemoodClient.java
@@ -0,0 +1,24 @@
+package com.eternity.jessemood.client;
+
+import com.eternity.jessemood.client.mixin.PlayerMixin;
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
+import net.minecraft.client.gui.DrawContext;
+import net.minecraft.client.render.RenderTickCounter;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.sound.SoundEvent;
+import net.minecraft.util.Identifier;
+
+public class JessemoodClient implements ClientModInitializer {
+    public static final SoundEvent JESUS_BELL_SOUND = Registry.register(Registries.SOUND_EVENT, Identifier.of("eternaljesus", "jesus_bell"),
+            SoundEvent.of(Identifier.of("eternaljesus", "jesus_bell")));
+
+    @Override
+    public void onInitializeClient() {
+        guihandler.init();
+        HudRenderCallback.EVENT.register((DrawContext context, float counter) -> {
+        guihandler.render (context);
+    });
+    }
+}
---
+++ src/main/java/com/eternity/jessemood/Jessemood.java
@@ -0,0 +1,10 @@
+package com.eternity.jessemood;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Jessemood implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
